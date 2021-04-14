7254017e0aaf59993029114c4372d727eafd770f
yuki_takahashi <yuk0406@icloud.com>
2021-04-14 20:13:05 +0900

    category用seederファイルを作成し、ダミーデータを挿入しました

---
+++ database/seeds/categoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class categoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+            'name' => '青春',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+            
+            'name' => '恋愛',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+    
+}
