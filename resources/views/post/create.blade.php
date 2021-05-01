83a7aefa3ab265069a3e15a60c07daf75eacaddb
yuki_takahashi <yuk0406@icloud.com>
2021-05-01 18:31:45 +0900

    微修正

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -22,11 +22,11 @@
                         <div class="form-group">
                             <label class="control-label">カテゴリー</label>
                             <select class="form-control" name="category_id">
-                            if(!is_null($categories))
+                            
                                 @foreach($categories as $category)
                                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                                 @endforeach
-                            @endif
+                           
                             </select>
                         </div>
                         <div class="form-group">
