c02d23c83d58542447e39dcdd0da4ce5182037b7
yuki_takahashi <yuk0406@icloud.com>
2021-05-07 22:16:46 +0900

    微修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Models\Category;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Auth;
 
 
