40a0432eef9fba6da5e61ebed129cb3327201a4e
yuki_takahashi <yuk0406@icloud.com>
2021-05-08 12:25:46 +0900

    微修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        $categories = Category::all();
+        $$categories = Category::all();
         return view('post.create', ['categories' => $categories]);
     }
 
