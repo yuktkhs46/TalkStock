52579eea8988acc8fe341bbfc1afbcacf4950dcf
yuki_takahashi <yuk0406@icloud.com>
2021-05-08 11:53:06 +0900

    categoriesテーブルを作成し直しました

--- app/Models/category.php
+++ app/Models/category.php
@@ -1,16 +1,14 @@
 <?php
 
 namespace App\Models;
-use App\Http\Controllers\PostController;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $guarded = array('id');
-    public function posts(){
-        return $this->hasMany(Post::class);
-    }
 
-
+    public static $rules = array(
+        'title' => 'required',
+    );
 }
--- database/migrations/2021_05_08_114419_create_categories_table.php
+++ database/migrations/2021_05_08_114419_create_categories_table.php
@@ -14,7 +14,8 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+       
+            $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
         });
