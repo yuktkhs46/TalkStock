183c2b8d06844beaccf724e4f98c7513044baf96
yuki_takahashi <yuk0406@icloud.com>
2021-05-17 19:38:57 +0900

    微修正

--- app/Models/category.php
+++ app/Models/category.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model 
+class Category extends Model
 {
     protected $guarded = array('id');
-
-    
     
     public function posts(){
         return $this->hasMany(Category::class);
