f6bd5509e9e1bdd5165693d005bd1129a67f20f0
yuki_takahashi <yuk0406@icloud.com>
2021-05-18 21:01:48 +0900

    微修正

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $guarded = array('id');
+    
+    // 投稿への紐付け
+    public function posts(){
+        return $this->hasMany(Category::class);
+
+    }
+}
